bae3cd843ea8c7cd781426bbc39cb2c27c25c2c6
Daniel <dflask13@gmail.com>
2018-09-03 11:56:49 +0100

    nc-news

--- listen.js
+++ listen.js
@@ -1,10 +1,5 @@
 const app = require("./app");
 
-<<<<<<< HEAD
 app.listen(9090, () => {
   console.log("listening on port 9090");
-=======
-app.listen(3000, () => {
-  console.log("listing on port 3000");
->>>>>>> e52fe74d96943a144a59883432ffa28dca682b1d
 });
