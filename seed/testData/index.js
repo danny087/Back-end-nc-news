650b27c959c9d7f292630deae0847a5ddc4f3158
Daniel <dflask13@gmail.com>
2018-09-03 11:55:10 +0100

    nc-news

--- seed/testData/index.js
+++ seed/testData/index.js
@@ -1,13 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
   articleData: require("../testData/articles.json"),
   commentData: require("../testData/comments.json"),
   topicData: require("../testData/topics.json"),
   userData: require("../testData/users.json")
-=======
-  articlesData: require("./articles.json"),
-  topicsData: require("./topics.json"),
-  commentsData: require("./comments.json"),
-  usersData: require("./users.json")
->>>>>>> e52fe74d96943a144a59883432ffa28dca682b1d
 };
